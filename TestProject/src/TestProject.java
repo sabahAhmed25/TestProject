36daa9a21ad8ca0d139a98c5f80a8ec5f6ad790a
sabah <sabah@DESKTOP-F68KP9U>
2022-10-17 00:40:34 +0530

    first

--- TestProject/src/TestProject.java
+++ TestProject/src/TestProject.java
@@ -1,4 +1,26 @@
 
 public class TestProject {
 
+	public class First {
+		static int a=100;
+		static int b=200;
+		static int c;
+		static void add()
+		{
+			c=a+b;
+			System.out.println("The sum is:"+c );
+		}
+		static void sub()
+		{
+			c=a-b;
+			System.out.println("The difference is:"+c);
+		}
+		public static void main(String[] args) {
+			
+			First obj=new First();
+			First.add();
+			First.sub();
+		}
+
+	}
 }
